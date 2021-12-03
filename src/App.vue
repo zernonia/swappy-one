e10153e7487cf394da241866881c32d42c5ea6d2
zernonia <59365435+zernonia@users.noreply.github.com>
2021-12-04 00:30:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-screen h-screen">
+    <h1 class="text-2xl font-semibold">Supabase Postman</h1>
+
+    <router-view></router-view>
+  </div>
+</template>
